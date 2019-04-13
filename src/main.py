bfce280a4e561b891e08cb2313f93c7c5ae6703a
Henrike100 <henrikem100@gmail.com>
2019-04-13 01:49:06 -0300

    Adicionando funcoes de leitura e escrita de arquivo

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
 from tkinter import *
+from tkinter import filedialog
 from funcoes import *
+
+bg_color = "white"
+button_color = "cyan"
+button_font = ("Arial", "15", "bold")
+text_font = ("Arial", "20", "bold")
 
 def verif_placa(placa):
     pass
@@ -11,104 +17,123 @@ class Interface:
         self.ordenado = False
         self.topo = Frame(instancia_Tk)
         self.topo["pady"] = 50
+        self.topo.config(background=bg_color)
         self.topo.pack()
         self.frame1 = Frame(instancia_Tk)
         self.frame1["pady"] = 6
+        self.frame1.config(background=bg_color)
         self.frame1.pack()
         self.frame2 = Frame(instancia_Tk)
         self.frame2["pady"] = 6
+        self.frame2.config(background=bg_color)
         self.frame2.pack()
         self.frame3 = Frame(instancia_Tk)
         self.frame3["pady"] = 6
+        self.frame3.config(background=bg_color)
         self.frame3.pack()
         self.frame4 = Frame(instancia_Tk)
         self.frame4["pady"] = 6
+        self.frame4.config(background=bg_color)
         self.frame4.pack()
         self.frame5 = Frame(instancia_Tk)
         self.frame5["pady"] = 6
+        self.frame5.config(background=bg_color)
         self.frame5.pack()
 
         self.msg1 = Label(self.topo, text = "Quantidade de Registros: 0")
         self.msg1["padx"] = 150
-        self.msg1["font"] = ("Arial", "20", "bold")
+        self.msg1["font"] = text_font
+        self.msg1.config(background=bg_color)
         self.msg1.pack(side=LEFT)
 
         self.msg2 = Label(self.topo, text = "Tipo de Ordenacao: Nenhuma")
         self.msg2["padx"] = 150
-        self.msg2["font"] = ("Arial", "20", "bold")
+        self.msg2["font"] = text_font
+        self.msg2.config(background=bg_color)
         self.msg2.pack(side=RIGHT)
 
-        self.B1 = Button(self.frame1, text="Gerar Registros Aleatoriamente", width=55)
-        self.B1["font"] = ("Arial", "15", "bold")
+        self.B1 = Button(self.frame1, text="Gerar Registros Aleatoriamente", width=55, bg=button_color)
+        self.B1["font"] = button_font
         self.B1.pack(side=LEFT)
         self.B1["command"] = self.gerar_regist_aleat
 
-        self.B2 = Button(self.frame1, text="Cadastrar Registro Individual", width=55)
-        self.B2["font"] = ("Arial", "15", "bold")
+        self.B2 = Button(self.frame1, text="Cadastrar Registro Individual", width=55, bg=button_color)
+        self.B2["font"] = button_font
         self.B2.pack(side=RIGHT)
         self.B2["command"] = self.cadastro
 
-        self.B3 = Button(self.frame2, text="Ordenar", width=112)
-        self.B3["font"] = ("Arial", "15", "bold")
+        self.B3 = Button(self.frame2, text="Ordenar", width=112, bg=button_color)
+        self.B3["font"] = button_font
         self.B3.pack(side=LEFT)
         self.B3["command"] = self.ordenar
 
-        self.B4 = Button(self.frame3, text="Mostrar Registros (Desordenado)", width=55)
-        self.B4["font"] = ("Arial", "15", "bold")
+        self.B4 = Button(self.frame3, text="Mostrar Registros (Desordenado)", width=55, bg=button_color)
+        self.B4["font"] = button_font
         self.B4.pack(side=LEFT)
         self.B4["command"] = lambda: mostrar_registros(self.desordenado, len(self.desordenado)) # Mudar
 
-        self.B5 = Button(self.frame3, text="Mostrar Registros (Ordenado)", width=55)
-        self.B5["font"] = ("Arial", "15", "bold")
+        self.B5 = Button(self.frame3, text="Mostrar Registros (Ordenado)", width=55, bg=button_color)
+        self.B5["font"] = button_font
         self.B5.pack(side=RIGHT)
         self.B5["command"] = lambda: mostrar_registros(self.registros, len(self.registros)) # Mudar
 
-        self.B6 = Button(self.frame4, text="Comparar Metodos de Ordenacao (Registro atual)", width=55)
-        self.B6["font"] = ("Arial", "15", "bold")
+        self.B6 = Button(self.frame4, text="Comparar Metodos de Ordenacao (Registro atual)", width=55, bg=button_color)
+        self.B6["font"] = button_font
         self.B6.pack(side=LEFT)
         self.B6["command"] = lambda: comparar_ordenacoes(self.registros, self.desordenado)
 
-        self.B7 = Button(self.frame4, text="Comparar Metodos de Ordenacao (Varios Registros Aleatorios)", width=55)
-        self.B7["font"] = ("Arial", "15", "bold")
+        self.B7 = Button(self.frame4, text="Comparar Metodos de Ordenacao (Varios Registros Aleatorios)", width=55, bg=button_color)
+        self.B7["font"] = button_font
         self.B7.pack(side=RIGHT)
         #self.B7["command"] = lambda: 
 
-        self.B8 = Button(self.frame5, text="Ler Registros de Arquivo", width=55)
-        self.B8["font"] = ("Arial", "15", "bold")
+        self.B8 = Button(self.frame5, text="Ler Registros de Arquivo", width=55, bg=button_color)
+        self.B8["font"] = button_font
         self.B8.pack(side=LEFT)
-        #self.B8["command"] = lambda: 
+        self.B8["command"] = self.abre_arquivo
 
-        self.B9 = Button(self.frame5, text="Salvar Registros em Arquivo", width=55)
-        self.B9["font"] = ("Arial", "15", "bold")
+        self.B9 = Button(self.frame5, text="Salvar Registros em Arquivo", width=55, bg=button_color)
+        self.B9["font"] = button_font
         self.B9.pack(side=RIGHT)
-        #self.B9["command"] = lambda: 
+        self.B9["command"] = self.salva_arquivo
     
 
     def gerar_regist_aleat(self):
         tela = Tk()
         tela.title('Gerar Registros Aleatoriamente')
+        texto = Frame(tela)
         campo = Frame(tela)
-        confirmacao = Frame(tela)
-        campo["pady"] = 30
-        confirmacao["pady"] = 5
+        botoes = Frame(tela)
+        msg = Frame(tela)
+        texto["pady"] = 10
+        campo["pady"] = 10
+        botoes["pady"] = 10
+        msg["pady"] = 10
+        texto.pack()
         campo.pack()
-        confirmacao.pack()
+        botoes.pack()
+        msg.pack()
 
-        text = Label(campo, text="Digite quantos registros voce quer criar")
-        text["font"] = ("Arial", "15")
-        text.grid(row=0, pady=5)
+        text = Label(texto, text="Digite quantos registros voce quer criar")
+        text["font"] = text_font
+        text.pack()
+
         valor = Entry(campo)
-        valor.grid(row=1, pady=5)
+        valor.pack()
 
-        mensagem = Label(confirmacao, text="")
+        mensagem = Label(msg, text=" ")
         mensagem["font"] = ("Arial", "10")
+        mensagem.pack()
 
-        botao = Button(confirmacao, text="ENVIAR", height=1)
-        botao["command"] = lambda: self.verif_valor(valor.get(), mensagem, tela)
-        botao.grid(row=2, pady=5)
-        mensagem.grid(row=3, pady=5)
+        botaoCancel = Button(botoes, text="CANCELAR", height=1, bg='red2')
+        botaoCancel["command"] = tela.destroy
+        botaoCancel.pack(side=LEFT)
 
-        tela.geometry("400x200+700+400")
+        botaoSend = Button(botoes, text="ENVIAR", height=1, bg='green2')
+        botaoSend["command"] = lambda: self.verif_valor(valor.get(), mensagem, tela)
+        botaoSend.pack(side=RIGHT)
+
+        tela.geometry("600x200+700+400")
         tela.mainloop()
     
 
@@ -130,46 +155,69 @@ class Interface:
     def cadastro(self):
         tela = Tk()
         tela.title('Cadastrar Registro Individual')
-        top = Frame(tela)
-        middle = Frame(tela)
-        bottom = Frame(tela)
-        top.pack()
-        middle.pack()
-        bottom.pack()
+        texto = Frame(tela)
+        frame1 = Frame(tela)
+        frame2 = Frame(tela)
+        frame3 = Frame(tela)
+        frame4 = Frame(tela)
+        botoes = Frame(tela)
+        msg = Frame(tela)
+        texto["pady"] = 10
+        frame1["pady"] = 10
+        frame2["pady"] = 10
+        frame3["pady"] = 10
+        frame4["pady"] = 10
+        botoes["pady"] = 10
+        msg["pady"] = 10
+        texto.pack()
+        frame1.pack()
+        frame2.pack()
+        frame3.pack()
+        frame4.pack()
+        botoes.pack()
+        msg.pack()
 
-        text = Label(top, text="Digite quantos registros voce quer criar")
+        text = Label(texto, text="Digite quantos registros voce quer criar")
+        text["font"] = text_font
         text["pady"] = 10
         text.pack()
 
-        anotext = Label(middle, text="Ano:")
-        anotext.grid(row=0, column=0, pady=10)
-        ano = Entry(middle)
-        ano.grid(row=0, column=1, pady=10)
+        anotext = Label(frame1, text="Ano: ")
+        anotext["padx"] = 13
+        anotext.pack(side=LEFT)
+        ano = Entry(frame1)
+        ano.pack(side=RIGHT)
 
-        placatext = Label(middle, text="Placa:")
-        placatext.grid(row=1, column=0, pady=10)
-        placa = Entry(middle)
-        placa.grid(row=1, column=1, pady=10)
+        placatext = Label(frame2, text="Placa: ")
+        placatext["padx"] = 8
+        placatext.pack(side=LEFT)
+        placa = Entry(frame2)
+        placa.pack(side=RIGHT)
 
-        donotext = Label(middle, text="Dono:")
-        donotext.grid(row=2, column=0, pady=10)
-        dono = Entry(middle)
-        dono.grid(row=2, column=1, pady=10)
+        donotext = Label(frame3, text="Dono: ")
+        donotext["padx"] = 8
+        donotext.pack(side=LEFT)
+        dono = Entry(frame3)
+        dono.pack(side=RIGHT)
 
-        modelotext = Label(middle, text="Modelo:")
-        modelotext.grid(row=3, column=0, pady=10)
-        modelo = Entry(middle)
-        modelo.grid(row=3, column=1, pady=10)
+        modelotext = Label(frame4, text="Modelo: ")
+        modelotext.pack(side=LEFT)
+        modelo = Entry(frame4)
+        modelo.pack(side=RIGHT)
 
-        mensagem = Label(bottom, text="")
+        mensagem = Label(msg, text=" ")
         mensagem["font"] = ("Arial", "10")
+        mensagem.pack()
 
-        botao = Button(bottom, text="ENVIAR", height=1)
+        botaoCancel = Button(botoes, text="CANCELAR", height=1, bg='red2')
+        botaoCancel["command"] = tela.destroy
+        botaoCancel.pack(side=LEFT)
+
+        botao = Button(botoes, text="ENVIAR", height=1, bg='green2')
         botao["command"] = lambda: self.verif_cadastro(ano.get(), placa.get(), dono.get(), modelo.get(), mensagem, tela)
-        botao.grid(row=0, pady=5)
-        mensagem.grid(row=1, pady=5)
+        botao.pack(side=RIGHT)
 
-        tela.geometry("400x300+400+400")
+        tela.geometry("650x350+400+400")
         tela.mainloop()
     
 
@@ -248,11 +296,60 @@ class Interface:
 
         tela.geometry("400x270+400+400")
         tela.mainloop()
+    
 
+    def abre_arquivo(self):
+        path = filedialog.askopenfilename(initialdir = "/",title = "Selecione o Arquivo",filetypes = [("eda2 files","*.eda2")])
+
+        i = 0
+        ano = 0
+        placa = ""
+        dono = ""
+        modelo = ""
+        try:
+            with open(path, 'r') as arq:
+                for linha in arq:
+                    linha = linha.strip()
+                    if i == 0:
+                        ano = int(linha)
+                    elif i == 1:
+                        placa = linha
+                    elif i == 2:
+                        dono = linha
+                    else:
+                        modelo = linha
+                
+                    i += 1
+
+                    if i == 4:
+                        self.registros.append(Registro(ano, placa, dono, modelo, len(self.registros)+1))
+                        i = 0
+
+            arq.close()
+        except:
+            return
+
+
+    def salva_arquivo(self):
+        path = filedialog.asksaveasfilename(initialdir = "/",title = "Selecione o Local para Salvar",filetypes = [("eda2 files","*.eda2")])
+        
+        try:
+            arq = open(path, 'w')
+
+            for i in range(len(self.registros)):
+                arq.write('{}\n'.format(self.registros[i].ano))
+                arq.write('{}\n'.format(self.registros[i].placa))
+                arq.write('{}\n'.format(self.registros[i].dono))
+                arq.write('{}\n'.format(self.registros[i].modelo))
+        
+            arq.close()
+        except:
+            return
 
 if __name__ == '__main__':
     menu=Tk()
     menu.title('LISTA 3 - ESTRUTURA DE DADOS 2')
+    menu.config(background=bg_color)
     menu.geometry("1400x450+300+200")
     Interface(menu)
     menu.mainloop()
